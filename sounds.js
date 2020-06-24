6edb8278d8cee4843492ad0ef322f867ae736e2f
jamilvice <45555877+jamilvice@users.noreply.github.com>
2020-06-24 23:14:35 +0200

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,22 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+    0,
+    9,
+    12,
+    15,
+    16.5,
+    24,
+    25.8,
+    31,
+    32,
+    34,
+    58,
+    67,
+    75,
+    81,
+    87,
+    105,
+    116,
+    130
+];
